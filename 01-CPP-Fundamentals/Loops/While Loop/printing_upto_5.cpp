f71fb935ed2d18b02d0474156542b9956a7628ac
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:04:17 +0400

    printing_upto_5.cpp

--- 01-CPP-Fundamentals/Loops/While Loop/printing_upto_5.cpp
+++ 01-CPP-Fundamentals/Loops/While Loop/printing_upto_5.cpp
@@ -1,1 +1,12 @@
+//While Function (Printing upto 5)
+#include <iostream>
+using namespace std;
 
+int main(){
+    int i = 0;
+    while (i <= 5){
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
