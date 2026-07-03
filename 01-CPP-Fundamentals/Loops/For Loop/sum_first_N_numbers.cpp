c630c7c6dc99f58dc35b0e719e245acf2314af10
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:03:05 +0400

    sum_first_N_numbers.cpp

--- 01-CPP-Fundamentals/Loops/For Loop/sum_first_N_numbers.cpp
+++ 01-CPP-Fundamentals/Loops/For Loop/sum_first_N_numbers.cpp
@@ -1,1 +1,15 @@
+//For Function (Sum of 1st N numbers entered by user)
+#include <iostream>
+using namespace std;
 
+int main(){
+    int sum = 0;
+    int n = 0;
+    cout << "Enter a number = " << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        sum = sum + i;
+    }
+    cout << "Sum of 1st " << n << " numbers = " << sum << endl;
+    return 0;
+}
