b341fc694e60b3b2bcc3015c78bb69e215deb9c0
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:38:40 +0400

    linear_search.cpp

--- 02-Arrays/Search Operations/linear_search.cpp
+++ 02-Arrays/Search Operations/linear_search.cpp
@@ -1,1 +1,17 @@
+//LINEAR SEARCH ON ARRAY - Find the index of the element 8 (Time Complexity = O(n))
+#include <iostream>
+using namespace std;
 
+void searchElement(int dat[], int s, int val){
+    for (int i = 0; i < s; i++){
+        if (dat[i] == val){
+            cout << "Index of the element is " << i << endl;
+        }
+    }
+}
+int main(){
+    int arr[] = {4,2,7,8,1,2,5};
+    int size = 7;
+    int element = 8;
+    searchElement(arr, size, element);
+}
