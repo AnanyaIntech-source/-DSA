970ec952d6e772f2a422a882e0340d9c27e25a7a
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:56:28 +0400

    read_all_words.cpp

--- 03-Strings/Basics/read_all_words.cpp
+++ 03-Strings/Basics/read_all_words.cpp
@@ -1,1 +1,12 @@
+//Read All Words - STRING
+#include <iostream>
+using namespace std;
+
+int main(){
+    string str;
+    while (cin >> str){
+        cout << str << " ";
+    }
+    return 0;
+}
 
