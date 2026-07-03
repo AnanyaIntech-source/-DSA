ed17b42fe624848b3d207c36213dfafaa19dcdf4
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:42:37 +0400

    reverse_print_array.cpp

--- 02-Arrays/Reverse/reverse_print_array.cpp
+++ 02-Arrays/Reverse/reverse_print_array.cpp
@@ -1,1 +1,12 @@
+//REVERSE PRINT ARRAY
+#include <iostream>
+using namespace std;
 
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    for (int i = size - 1; i >= 0; i--){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
