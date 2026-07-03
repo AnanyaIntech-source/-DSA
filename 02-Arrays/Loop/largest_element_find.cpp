a031b6bb02a3b335089ab3ed2503bb84b4db61dd
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:34:09 +0400

    largest_element_find.cpp

--- 02-Arrays/Loop/largest_element_find.cpp
+++ 02-Arrays/Loop/largest_element_find.cpp
@@ -1,1 +1,18 @@
+//LARGEST element in ARRAY using LOOP
+#include <iostream>
+#include <climits>
+using namespace std;
 
+int main(){
+    int arr[] = {5, 15, 22, 1, -15, -24};
+    int largest = INT_MIN;
+    
+    for (int i = 0; i < 6; i++){
+        if (arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+
+    cout << "Largest element in array is >> " << largest << endl;
+    return 0;
+}
