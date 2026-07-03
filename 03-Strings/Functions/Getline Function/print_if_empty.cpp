c963917675b4973e6faa976835d9f6c3a2ef95d7
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:59:56 +0400

    print_if_empty.cpp

--- 03-Strings/Functions/Getline Function/print_if_empty.cpp
+++ 03-Strings/Functions/Getline Function/print_if_empty.cpp
@@ -1,1 +1,17 @@
+//Print if empty string using getline
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main(){
+    string line;
+    while (getline(cin,line)){
+        if (!line.empty()){
+            cout << "The string is " << line << endl;
+        }
+        else {
+            cout << "The string is empty!" << endl;
+        }
+    }
+    return 0;
+}
