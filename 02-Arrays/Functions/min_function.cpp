cefd74cbb94e3079a92b87056cde760abb22fd92
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:35:23 +0400

    min_function.cpp

--- 02-Arrays/Functions/min_function.cpp
+++ 02-Arrays/Functions/min_function.cpp
@@ -1,1 +1,16 @@
+//SMALLEST element in ARRAY using min FUNCTION
+#include <iostream>
+#include <climits>
+using namespace std;
 
+int main(){
+    int arr[] = {5, 15, 22, 1, -15, -24};
+    int smallest = INT_MAX;
+    
+    for (int i = 0; i < 6; i++){
+        smallest = min(arr[i],smallest);
+    }
+
+    cout << "Smallest element in array is >> " << smallest << endl;
+    return 0;
+}
