48594c84606b41932805b848d68be3c5693ca4f7
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:08:09 +0400

    if_else.cpp

--- 01-CPP-Fundamentals/Conditionals/if_else.cpp
+++ 01-CPP-Fundamentals/Conditionals/if_else.cpp
@@ -1,1 +1,16 @@
+//If-Else (Check if user input number greater than 10)
+#include <iostream>
+using namespace std;
 
+int main(){
+    int num = 0;
+    cout << "Enter a Number = " << endl;
+    cin >> num;
+    if (num > 10){
+        cout << "Number greater than 10" << endl;
+    }
+    else{
+        cout << "Number lesser than 10" << endl;
+    }
+    return 0;
+}
