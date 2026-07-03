0759d28c2d6a52578a39b2fb866c3797af0c8a71
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:36:14 +0400

    max_function.cpp

--- 02-Arrays/Functions/max_function.cpp
+++ 02-Arrays/Functions/max_function.cpp
@@ -1,1 +1,16 @@
+//LARGEST element in ARRAY using max FUNCTION
+#include <iostream>
+#include <climits>
+using namespace std;
 
+int main(){
+    int arr[] = {5, 15, 22, 1, -15, -24};
+    int largest = INT_MIN;
+    
+    for (int i = 0; i < 6; i++){
+        largest = max(arr[i], largest);
+    }
+
+    cout << "Largest element in array is >> " << largest << endl;
+    return 0;
+}
