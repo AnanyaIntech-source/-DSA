f1884f10f6e197d4d6cf92144ec46b69303fb928
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:37:13 +0400

    getline_function.cpp

--- 02-Arrays/Functions/getline_function.cpp
+++ 02-Arrays/Functions/getline_function.cpp
@@ -1,1 +1,28 @@
+//Continuously takes string input from the user using getline()
+//Stops when the user enters an empty line
+//For every non-empty string:
+//                          print the string inside [ ]
+//                          print its length
+//                          check whether its length is greater than 5 or not
+#include <iostream>
+#include <string>
+#include <climits>
+using namespace std;
 
+int main(){
+    string str;
+    while (getline(cin, str)){
+        if (str.empty()){
+            break;
+        }
+        cout << "[" << str << "]" << endl;
+        cout << "Length = " << str.size() << endl;
+        if (str.size()>5){
+            cout << "Large String" << endl;
+        }
+        else{
+            cout << "Small String" << endl;
+        }
+    }
+    return 0;
+}
