68d2cdab646ded4d836195ad91b7706d042c5511
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:28:23 +0400

    updating_element.cpp

--- 02-Arrays/updating_element.cpp
+++ 02-Arrays/updating_element.cpp
@@ -1,1 +1,15 @@
+//Updating Element in ARRAY
+#include <iostream>
+using namespace std;
 
+int main(){
+    int arr[5] = {99, 90, 92, 94, 95};
+    arr[0] = 101;
+    cout << "Updated Marks >> " << endl;
+    cout << arr[0] << endl;
+    cout << arr[1] << endl;
+    cout << arr[2] << endl;
+    cout << arr[3] << endl;
+    cout << arr[4] << endl;
+    return 0;
+}
