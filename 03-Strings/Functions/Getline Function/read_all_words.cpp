1fd19cbf9c9458d46fe8f87a50b372e91ecc2256
Ananya Siju <ananyasiju16@gmail.com>
2026-07-03 17:58:30 +0400

    read_all_words.cpp

--- 03-Strings/Functions/Getline Function/read_all_words.cpp
+++ 03-Strings/Functions/Getline Function/read_all_words.cpp
@@ -1,1 +1,12 @@
+//Read All Words - STRING USING GETLINE
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main(){
+    string str;
+    while (getline(cin,str)){
+        cout << str << " ";
+    }
+    return 0;
+}
