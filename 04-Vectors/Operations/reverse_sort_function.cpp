ad8a56e286212a36f4490ddcb78ce43926d7fae2
Ananya Siju <ananyasiju16@gmail.com>
2026-07-04 10:20:52 +0400

    Update reverse_sort_function.cpp

--- 04-Vectors/Operations/reverse_sort_function.cpp
+++ 04-Vectors/Operations/reverse_sort_function.cpp
@@ -1,1 +1,19 @@
+//REVERSE SORT FUNCTION
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main(){
+    vector<int> v = {5,2,3,1,4};
+    cout << "Before Sorting " << endl;
+    for (int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    sort(v.rbegin(),v.rend());
+    cout << endl << "After Sorting " << endl;
+    for (int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    return 0;
+}
