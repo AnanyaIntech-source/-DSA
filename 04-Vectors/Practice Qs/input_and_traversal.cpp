430c3f8a75ddf7f48bd7f29464431c1462081403
Ananya Siju <ananyasiju16@gmail.com>
2026-07-04 10:24:25 +0400

    Update input_and_traversal.cpp

--- 04-Vectors/Practice Qs/input_and_traversal.cpp
+++ 04-Vectors/Practice Qs/input_and_traversal.cpp
@@ -1,1 +1,20 @@
+//Write a C++ program to input N integers from the user into a vector 
+//and print all elements of the vector
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main(){
+    vector<int> v;
+    cout << "Enter elements = " << endl;
+    int N;
+    while (cin >> N){
+        v.push_back(N);
+    }
+    cout << "Final Vector = " << endl;
+    for (int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    return 0;
+}
