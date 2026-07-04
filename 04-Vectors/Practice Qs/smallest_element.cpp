78ba5da9f465391bbe68e54dd7b1c8795469c1aa
Ananya Siju <ananyasiju16@gmail.com>
2026-07-04 17:24:34 +0400

    Update smallest_element.cpp

--- 04-Vectors/Practice Qs/smallest_element.cpp
+++ 04-Vectors/Practice Qs/smallest_element.cpp
@@ -1,1 +1,30 @@
+//SMALLEST ELEMENT IN VECTOR
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    int N;
+    cout << "Enter Number of Elements = ";
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    cout << "Orginial Vector is ";
+    for (int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    int smallest = INT_MAX;
+    for (int i = 0; i < v.size(); i++){
+        if (v[i] < smallest){
+            smallest = v[i];
+        }
+    }
+    cout << endl << "SMallest element = " << smallest << endl;
+    return 0;
+}
 
