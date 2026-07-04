1ab94e8c7b30ebc4afcc763a60cff27712a0a496
Ananya Siju <ananyasiju16@gmail.com>
2026-07-04 17:28:14 +0400

    Update merge_vectors.cpp

--- 04-Vectors/Practice Qs/merge_vectors.cpp
+++ 04-Vectors/Practice Qs/merge_vectors.cpp
@@ -1,1 +1,23 @@
+//Merge Vectors
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main(){
+    vector<int> v1 = {1,2,3,4,5};
+    vector<int> v2 = {5,4,3,2,1};
+    cout << "Orginial Vector1 is ";
+    for (int i = 0; i < v1.size(); i++){
+        cout << v1[i] << " ";
+    }
+    cout << endl << "Orginial Vector2 is ";
+    for (int i = 0; i < v2.size(); i++){
+        cout << v2[i] << " ";
+    }
+    v1.insert(v1.end(), v2.begin(), v2.end());
+    cout << endl << "New Vector1 is ";
+    for (int i = 0; i < v1.size(); i++){
+        cout << v1[i] << " ";
+    }
+    return 0;
+}
