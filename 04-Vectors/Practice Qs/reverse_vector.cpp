976555972911f2336aceb11bd78107becf922bdd
Ananya Siju <ananyasiju16@gmail.com>
2026-07-04 17:23:38 +0400

    Update reverse_vector.cpp

--- 04-Vectors/Practice Qs/reverse_vector.cpp
+++ 04-Vectors/Practice Qs/reverse_vector.cpp
@@ -1,1 +1,35 @@
+//REVERSE VECTOR
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+void revVector(vector<int>& vec){
+    int start = 0, end = vec.size() - 1;
+    while (start < end){
+        swap(vec[start],vec[end]);
+        start++;
+        end--;
+    }
+}
+int main(){
+    vector<int> v;
+    int N;
+    cout << "Enter Number of Elements = ";
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    cout << "Orginial Vector is ";
+    for (int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl << "Vector in reverse is "; 
+    revVector(v);
+    for (int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    return 0;
+}
