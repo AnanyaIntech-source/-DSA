04bdd2cfc359530491b77a6dcc996cfa800753e0
Ananya Siju <ananyasiju16@gmail.com>
2026-07-04 10:05:37 +0400

    Update copy_string.cpp

--- 03-Strings/Functions/copy_string.cpp
+++ 03-Strings/Functions/copy_string.cpp
@@ -1,1 +1,10 @@
+//Copying String
+#include <iostream>
+using namespace std;
 
+int main(){
+    string l = "dev";
+    string copy = l;
+    cout << "Copied line is >> " << copy << endl;
+    return 0;
+}
