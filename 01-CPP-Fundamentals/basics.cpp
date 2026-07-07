b6216f50b57e7b9b1ad88b9fa7796a5b1976df2a
Ananya Siju <ananyasiju16@gmail.com>
2026-07-07 20:02:25 +0400

    Update basics.cpp

--- 01-CPP-Fundamentals/basics.cpp
+++ 01-CPP-Fundamentals/basics.cpp
@@ -1,1 +1,24 @@
+//-------------------------------BASE------------------------------------------
+//Simple Program Base
+#include <iostream>
+using namespace std;
 
+int main(){
+    cout << "Welcome to C++ world";
+    return 0;
+}
+
+
+// Basic Operations
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Enter 2 numbers = " << endl;
+    int n1 = 0, n2 = 0;
+    cin >> n1 >> n2;
+    cout << "Sum of " << n1 << " and " << n2 << " are " << n1 + n2 << endl;
+    cout << "Difference of " << n1 << " and " << n2 << " are " << n1 - n2 << endl;
+    cout << "Product of " << n1 << " and " << n2 << " are " << n1*n2 << endl;
+    return 0;
+}
